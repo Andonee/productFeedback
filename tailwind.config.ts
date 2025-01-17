a7a04ebc47b6d76de8f145205d8e224ab247bdcd
Mateusz Poprawa <mateusz.poprawa@gmail.com>
2025-01-17 08:21:46 +0100

    Add UI components (#1)
    * add custom button
    
    * add select input
    
    * add input
    
    * add textarea

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -10,13 +10,22 @@ export default {
   theme: {
     extend: {
       colors: {
-        background: 'var(--background)',
-        foreground: 'var(--foreground)',
+        text: 'var(--text)',
+        promiscuousPink: 'hsl(var(--promiscuous-pink) / <alpha-value>)',
+        bluetiful: 'hsl(var(--bluetiful) / <alpha-value>)',
+        ravenNight: 'hsl(var(--raven-night) / <alpha-value>)',
+        snowy: 'hsl(var(--snowy) / <alpha-value>)',
+        cottonBall: 'hsl(var(--cotton-ball) / <alpha-value>)',
+        ghostWhite: 'hsl(var(--ghost-white) / <alpha-value>)',
+        jewelCave: 'hsl(var(--jewel-cave) / <alpha-value>)',
+        oceanNight: 'hsl(var(--ocean-night) / <alpha-value>)',
+        creamyPeach: 'hsl(var(--creamy-peach) / <alpha-value>)',
+        blueMana: 'hsl(var(--blue-mana) / <alpha-value>)',
       },
       borderRadius: {
         lg: 'var(--radius)',
         md: 'calc(var(--radius) - 2px)',
-        sm: 'calc(var(--radius) - 4px)',
+        sm: 'var(--radius-sm)',
       },
     },
   },
