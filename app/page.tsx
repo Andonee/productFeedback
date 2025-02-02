ae2283e5eb658bea88e695a7c932c9c17c8a125d
Andonee <mateusz.poprawa@gmail.com>
2025-02-02 11:11:16 +0100

    add mobile menu

--- app/page.tsx
+++ app/page.tsx
@@ -4,16 +4,16 @@ import TagsCard from './components/TagsCard'
 
 export default function Home() {
   return (
-    <>
-      <div className='col-span-12 md:col-span-4 xl:col-span-3'>
+    <main className='grid grid-cols-12 gap-4'>
+      <div className='col-span-12 hidden md:col-span-4 md:block xl:col-span-3'>
         <HeaderCard />
       </div>
-      <div className='md:col-start-5 md:col-end-9 xl:col-start-1 xl:col-end-4'>
+      <div className='hidden md:col-start-5 md:col-end-9 md:block xl:col-start-1 xl:col-end-4'>
         <TagsCard />
       </div>
-      <div className='md:col-start-9 md:col-end-13 xl:col-start-1 xl:col-end-4'>
+      <div className='hidden md:col-start-9 md:col-end-13 md:block xl:col-start-1 xl:col-end-4'>
         <RoadmapCard />
       </div>
-    </>
+    </main>
   )
 }
