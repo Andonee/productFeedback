ad286674b4422e7fc7d81134671d995b24708987
Andonee <mateusz.poprawa@gmail.com>
2025-02-17 20:22:33 +0100

    add read suggestion page

--- app/components/SuggestionList/SuggestionList.tsx
+++ app/components/SuggestionList/SuggestionList.tsx
@@ -1,13 +1,13 @@
-import NoSuggestions from './NoSuggestions'
+import Suggestion from './Suggestion'
 
 const SuggestionList = () => {
   return (
     <div className='flex h-full flex-col gap-4'>
-      {/* <Suggestion />
       <Suggestion />
       <Suggestion />
-      <Suggestion /> */}
-      <NoSuggestions />
+      <Suggestion />
+      <Suggestion />
+      {/* <NoSuggestions /> */}
     </div>
   )
 }
